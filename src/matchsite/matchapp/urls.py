a39f5597ace24fedd8aeb95c6f6fc63e5087a4ac
Amair <a.naeem@se15.qmul.ac.uk>
2018-11-15 22:30:10 +0000

    url + setting

--- src/matchsite/matchapp/urls.py
+++ src/matchsite/matchapp/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    #signup 
+    path('signup/', views.signup, name='signup'),
+    #register
+    path('register/', views.register, name='register'),
+    #login page
+    path('login/', views.login, name='login'),
 ]
