064555d190b2ccb322559bff85865bbc28f967e3
Amair <a.naeem@se15.qmul.ac.uk>
2018-11-17 21:38:05 +0000

    updated url and view
    updated url and view

--- src/matchsite/matchapp/urls.py
+++ src/matchsite/matchapp/urls.py
@@ -8,6 +8,16 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     #register
     path('register/', views.register, name='register'),
+    #user profile edit page 
+    path('profile/', views.profile, name='profile'),
     #login page
     path('login/', views.login, name='login'),
+    #logout page
+    path('logout/', views.logout, name = 'logout'),
+    #similar hobbies
+    path('similarHobbies/', views.similarHobbies, name='similarHobbies'),
+    #Ajax: filter 
+    path('filter/', views.filter, name='filter'),
+    #upload image
+    path('uploadimage/', views.upload_image, name='uploadimage'),
 ]
--- src/matchsite/matchapp/views.py
+++ src/matchsite/matchapp/views.py
@@ -1,5 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from mainapp.models import Member, Profile, Hobby
 
+#user logged in
+def loggedin(request):
+	return HttpResponse("user logged in")
+
+#should render login page but also include a signup button
 def index(request):
-    return HttpResponse("Hello match app GIT")
+    return HttpResponse("Login page")
+
+#should render the signup page
+def signup(request):
+	return HttpResponse("signup page")
+
+#should render user registered page if unique user is entered
+#need validation for email, user, dob, profile image 
+def register(request):
+	if 'username' in request.POST and 'password' in request.POST:
+		u = request.POST['username']
+		passw = request.POST['password']
+		user = Member(username=u)
+		user.set_password(passw)
+		try:
+			user.save()
+
+		except: 
+			Http404("Username " + u "is already taken")
+
+		return HttpResponse("the user has been registered rendered page")
+
+	else:
+		return HttpResponse("no data was inserted")
+
+#this occurs when user presses login button from index
+def login(request):
+	return HttpResponse("login")
+
+#render logout page
+def logout(request):
+	return HttpResponse("logout")
+
+#shows another page with users that have similar interests 
+#order of most common hobbies first
+@loggedin
+def similarHobbies(request):
+	return HttpResponse("return list of people with similar hobbies")
+
+#filter button on similarHobbies page which generates
+@loggedin
+def filter(request):
+	return HttpResponse("filter by gender and age using Ajax")
+
+#user profile edit page
+@loggedin
+def profile(request):
+	return HttpResponse("user should be able to edit page")
+
+@loggedin
+def uploadImage(request):
+	return HttpResponse("user should be able to upload an image")
