fc74fb7ffe82abb38d8639d4911c694783ab60c6
Amair <a.naeem@se15.qmul.ac.uk>
2018-11-19 22:13:11 +0000

    Added ajax file and added ids for signup

---
+++ src/matchsite/matchapp/static/matchapp/ajax.js
@@ -0,0 +1,31 @@
+$.ajax({
+		
+		type: "POST",
+		data: {
+			  
+			  'csrfmiddlewaretoken' : $('input[name=csrfmiddlewaretoken]').val(),
+			   
+			   username: $("#username").val()
+		       
+		       password: $("#psw").val()
+
+		       gender: $("input[name='gender']:checked").val();
+
+   		       image: //need value of image
+
+		       email: $("#email").val()
+
+		   	  },
+
+		//https://www.youtube.com/watch?v=H1sHOvc8au0
+		success: function (response) {
+        	if (response.success == true) {
+        		alert("hey")
+        	}
+
+        	else {
+        		alert("no")
+        	}
+
+        }
+	 });
