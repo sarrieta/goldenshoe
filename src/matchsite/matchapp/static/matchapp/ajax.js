da656e448326bead2bd5f823eb5dff7d7f356fcd
Amair <a.naeem@se15.qmul.ac.uk>
2018-11-20 17:43:27 +0000

    Update Ajax

--- src/matchsite/matchapp/static/matchapp/ajax.js
+++ src/matchsite/matchapp/static/matchapp/ajax.js
@@ -1,31 +1,41 @@
-$.ajax({
+
+$(".signupbtn").click(function(event){
+	alert("test");
+/*$.ajax({
 		
 		type: "POST",
-		data: {
-			  
+		/*data: {
+
 			  'csrfmiddlewaretoken' : $('input[name=csrfmiddlewaretoken]').val(),
 			   
-			   username: $("#username").val()
+			   username: $("#username").val(),
 		       
-		       password: $("#psw").val()
+		       password: $("#psw").val(),
 
-		       gender: $("input[name='gender']:checked").val();
+		       gender: $("input[name='gender']:checked").val(),
 
    		       //image: //need value of image
 
 		       email: $("#email").val()
 
-		   	  },
+		   	  },*/
 
 		//https://www.youtube.com/watch?v=H1sHOvc8au0
-		success: function (response) {
-        	if (response.success == true) {
+		//success: console.log("test"),
+		//error: console.log("test")
+
+
+
+		/*function (response) {
+        	/*if (response.success == true) {
         		alert("hey")
         	}
 
         	else {
         		alert("no")
         	}
+        	console.log("test")
 
         }
-	 });
+	 });*/
+});
