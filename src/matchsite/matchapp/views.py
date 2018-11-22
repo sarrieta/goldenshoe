a418e2c7f29bf9ff81267be8c4da42640bdad2d3
Anas Adan <anas__adan@hotmail.com>
2018-11-22 23:43:45 +0000

    Some comments on hobbiesimilarities

--- src/matchsite/matchapp/views.py
+++ src/matchsite/matchapp/views.py
@@ -120,21 +120,26 @@ def logout(request,user):
 #order of most common hobbies first
 @loggedin
 def similarHobbies(request, user):
-    # Get all the other users exclude current logged in user
-    exclude = Member.objects.exclude(id=user.id)
-    # Filter based on the current logged in user on same hobbies
-    common = exclude.filter(hobbies__in=user.hobbies.all())
-    # Get the number of hobbies of other users
-    hobbies = common.annotate(hob_count=Count('hobbies'))
-    # Process the matches in decending
-    match = hobbies.filter(hob_count__gt=1).order_by('-hob_count')
+    # Check for the request type: 
+    if request.method == POST:
+        # Get all the other users exclude current logged in user
+        exclude = Member.objects.exclude(id=user.id)
+        # Filter based on the current logged in user on same hobbies
+        common = exclude.filter(hobbies__in=user.hobbies.all())
+        # Get the number of hobbies of other users
+        hobbies = common.annotate(hob_count=Count('hobbies'))
+        # Process the matches in decending
+        match = hobbies.filter(hob_count__gt=1).order_by('-hob_count')
     
-    context = { 
-        'appname' : appname,
-        'matches' : match,
-        'loggedIn': True
-        }
-    return render(request,'#',context)
+        context = { 
+            'appname' : appname,
+            'matches' : match,
+            'loggedIn': True
+            }
+        # might have return JSON data back?
+        #return render(request,'#',context)
+    else:
+        raise Http404('POST not used') 
 
 #filter button on similarHobbies page which generates
 @loggedin
