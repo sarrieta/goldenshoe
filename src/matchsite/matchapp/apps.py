383b9effd20090a8f79b3edf4b5223657001a101
Sergio X <sergio.arrieta.siyom@gmail.com>
2020-09-20 09:08:08 +0100

    make it better

--- src/matchsite/matchapp/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class MatchappConfig(AppConfig):
-    name = 'matchapp'
-
-    def ready(self):
-        import matchapp.signals.signals
--- src/matchsite/matchapp/templatetags/hobbies_count.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.simple_tag(takes_context=True)
-def get_number_of_hobbies(context, string):
-    string = str(string)
